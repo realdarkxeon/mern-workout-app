d20df964a01df78af6c3aa3baf45b327f3324f4b
realdarkxeon <realdarkxeon@gmail.com>
2022-10-24 18:36:59 +0600

    add main files

---
+++ frontend/src/hooks/useLogout.js
@@ -0,0 +1,22 @@
+import { useAuthContext } from "./useAuthContext";
+import { useWorkoutsContext } from "./useWorkoutsContext";
+
+export const useLogout = () => {
+    const {dispatch} = useAuthContext();
+    const {dispatch: workoutsDispatch} = useWorkoutsContext();
+
+    const logout = () => {
+        // Remove user from storage
+        localStorage.removeItem('user');
+        
+        dispatch({
+            type: 'LOGOUT'
+        });
+        workoutsDispatch({
+            type: 'SET_WORKOUTS',
+            payload: null
+        });
+    };
+
+    return {logout};
+};
